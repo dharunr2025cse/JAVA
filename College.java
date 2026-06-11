a9ef7b354871265dca7fc01d32a322d3a6badad6
dharun576-git <dharunravivarman1@gmail.com>
2026-06-11 15:23:19 +0530

    test commit

--- College.java
+++ College.java
@@ -10,17 +10,18 @@ class Details{
       this.regNo=rNo;
       this.studentName=name;
       this.percentage=percentage;
-    }
-    void display(){
-        System.out.println("Student Name:"+studentName);
-        System.out.println("Student Register No:"+regNo);
-        System.out.println("Student percentage:"+percentage);    
-    }
-}   class College{
-    public static void main(String[] args){
-        Details s1=new Details(10,"Dharun",93.55);
-        Details s2=new Details(12,"Bharat",84.88);
-        s1.display();
-        s2.display();
-     }
+    } 
 }
+//     void display(){
+//         System.out.println("Student Name:"+studentName);
+//         System.out.println("Student Register No:"+regNo);
+//         System.out.println("Student percentage:"+percentage);    
+//     }
+// }   class College{
+//     public static void main(String[] args){
+//         Details s1=new Details(10,"Dharun",93.55);
+//         Details s2=new Details(12,"Bharat",84.88);
+//         s1.display();
+//         s2.display();
+//      }
+// }
